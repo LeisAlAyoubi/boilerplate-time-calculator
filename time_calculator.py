ac96c8c509c402db63caf3cb3b629312ab33ad13
Leis Al Ayoubi <77484094+LeisAlAyoubi@users.noreply.github.com>
2021-07-04 20:35:15 +0000

    Initial commit

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,42 @@
 def add_time(start, duration):
+  #split into time and pm/am 
+  time, pm_am = start.split(" ")
+  start_hour, start_minutes = time.split(":")
+  duration_hours, duration_minutes = duration.split(":")
+  pm_am = pm_am.strip()
+  start_hour = start_hour.strip()
+  start_minutes = start_minutes.strip()
+
+  print(start_hour)
+  print(start_minutes)
+  print(duration_hours)
+  print(duration_minutes)
+  print(pm_am + "\n")
+
+  if(pm_am == "PM"):
+    new_minutes = int(start_minutes) + int(duration_minutes)
+    new_hour = int(start_hour) + int(duration_hours)
+    while(new_minutes > 60):
+      new_minutes = new_minutes - 60
+      new_hour = new_hour + 1
+    while(new_hour > 12):
+      new_hour = new_hour - 12
+      pm_am = "Am"
+    print(new_hour)
+    print(new_minutes)
+    print(pm_am + "\n")
+
+
+
+
+    #formating the output time
+    if(len(str(new_hour))<2 and len(str(new_minutes))==2):
+      new_time = "0" + str(new_hour) + ":" + str(new_minutes) + " " + pm_am
+    elif(len(str(new_hour))<2 and len(str(new_minutes))<2):
+      new_time = "0" + str(new_hour) + ":" + "0" + str(new_minutes) + " " + pm_am
 
 
 
 
 
-    return new_time
+  return new_time
