115fcd5baf70617f6033807187c356be0070e8d2
Leis Al Ayoubi <77484094+LeisAlAyoubi@users.noreply.github.com>
2021-07-06 07:30:58 +0000

    Added weekday counter (needs debugging)

--- time_calculator.py
+++ time_calculator.py
@@ -2,6 +2,7 @@ def add_time(start, duration, day =""):
   #split into time and pm/am 
   new_time=0
   next_day = False
+  next_weekday =""
   next_day_counter = 0
 
   #Get time and duration from input string
@@ -42,6 +43,24 @@ def add_time(start, duration, day =""):
   if(new_hour == 0):
     new_hour = 12
 
+        
+  weekdays = {"monday" : 1,
+  "tuesday" : 2, 
+  "wednesday" : 3,
+  "thursday" : 4,
+  "friday" : 5,
+  "saturday" : 6,
+  "sunday" : 7}
+
+  #calculating day of the week 
+  if(day != "" and next_day == True):
+    day = day.lower()
+    current_day_number = weekdays.get(day)
+    weekdays_plus = current_day_number + next_day_counter
+    while(weekdays_plus > 7):
+      weekdays_plus -= 7
+    next_weekday = list(weekdays.keys())[list(weekdays.values()).index(weekdays_plus)]
+
   #formating the output time
   if(len(str(new_hour))<2 and len(str(new_minutes))==2):
     new_time = "0" + str(new_hour) + ":" + str(new_minutes) + " " + pm_am
@@ -58,11 +77,11 @@ def add_time(start, duration, day =""):
   #Add weekday to output if specified and number of days later
   if(day != ""):
     new_time += ", " + day
-  elif(day != "" and next_day ==True and next_day_counter != 1):
-    new_time += ", " + day +"(" + next_day_counter + " days later)"
-  elif(day != "" and next_day ==True and next_day_counter == 1):
-    new_time += ", " + day +"(next day)"
-  elif(next_day ==True and next_day_counter != 1):
+  elif(next_weekday != "" and next_day == True and next_day_counter != 1):
+    new_time += ", " + next_weekday +"(" + next_day_counter + " days later)"
+  elif(next_weekday != "" and next_day == True and next_day_counter == 1):
+    new_time += ", " + next_weekday +"(next day)"
+  elif(next_day == True and next_day_counter != 1):
     new_time += " (" + str(next_day_counter) + " days later)"
   elif(next_day ==True and next_day_counter == 1):
     new_time +=" (next day)"
